2127c01591f685d02ff879ad5eed3646e51396f7
SkBadulla <skbadulla809@gmail.com>
2021-06-24 17:53:52 +0530

    Day26 files Updated

---
+++ Day-26/FirstProject/FirstApp/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Register
+
+# Create your views here.
+
+def home(request):
+	return HttpResponse("Hi Good Evening to All...")
+
+def htmltag(req):
+	return HttpResponse("<h2>Hi Welcome to APSSDC Programs</h2>")
+
+def usernameprint(request,uname):
+	return HttpResponse("<h2>Hi Welcome <span style='color:green'>{}</span></h2>".format(uname))
+
+def usernameage(request,un,ag):
+	return HttpResponse("<h3 style='text-align:center;background-color:green;padding:23px'>Hi User <span style='color:yellow'>{}</span> and your age is: <span style='color:red'>{}</span></h3>".format(un,ag))
+
+
+def empdetails(request,eid,ename,eage):
+	return HttpResponse("<script>alert('Hi Welcome {}')</script><h3>Hi Welcome {} and your age is: {} and your id is: {}</h3>".format(ename,ename,eage,eid))
+
+def htm(request):
+	return render(request,'html/sample.html')
+
+def ytname(request,name):
+	return render(request,'html/ytname.html',{'n':name})
+
+def empname(request,id,ename):
+	k = {'i':id,'n':ename}
+	return render(request,'html/ehtml.html',k)
+
+def studentdetails(request):
+	return render(request,'html/std.html')
+
+def internalJS(request):
+	return render(request,'html/internalJS.html')
+
+def myform(req):
+	if req.method=="POST":
+		#print(req.POST)
+		uname = req.POST['uname']
+		rollno = req.POST['rollno']
+		email = req.POST.get('email')
+		#print(uname,rollno,email)
+		data = {'username':uname,'rno':rollno,'emailId':email}
+		return render(req,'html/display.html',data)
+	return render(req,'html/myform.html')
+
+
+def bootstarpfun(request):
+	return render(request,'html/sampleboot.html')
+
+def btregi(request):
+	return render(request,'html/btregst.html')
+
+def register1(request):
+	# name = "siva"
+	# email = "siva@gmail.com"
+	reg = Register(name = "rasool",email="rasool@gmailcom")
+	reg.save()
+	return HttpResponse("row inserted successfully...")
+
+def register2(request):
+	if request.method == 'POST':
+		name = request.POST['name']
+		email = request.POST['email']
+		reg = Register(name = name, email = email)
+		reg.save()
+		return HttpResponse("Record inserted successfully....")
+	return render(request,"html/register2.html")
+
+def display(request):
+	data = Register.objects.all()
+	return render(request,'html/display1.html',{'data':data})
